5ddeabafa9b91a3c759cac20269b5aea32e58bca
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-06-17 18:00:23 +0200

    feat: print my random number into the html

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
+// recupero la variabile con il container html dei miei nuemri cauali
+const numContainer = document.querySelector(".num-container");
+
 // creo l'array dove verranno inseriti i 5 numeri casuali generati
 const randomNumArr = [];
 
-// creo il ciclo per gererare 5 numeri casuali da 1 a 100
+// richiamo la funzione per gererare 5 numeri casuali da 1 a 100 e pusharli nell array
 generateTotNumRandom(1, 100, 5, randomNumArr);
+
+// creo un ciclo per estrapolare i singoli elementi dell'array e stamparli in pagina
+for (let i = 0; i < randomNumArr.length; i++) {
+  let risultato = randomNumArr[i];
+  console.log(risultato);
+  numContainer.innerHTML += `<span class="random-num">${risultato}</span>`;
+}
