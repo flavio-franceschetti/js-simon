bd9577a5c946c05ba9229826ebb525091c8b525a
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-06-18 09:20:35 +0200

    feat: add game function, compare array and printm which and how much numbers are guessed

--- js/function.js
+++ js/function.js
@@ -15,3 +15,33 @@ function generateTotNumRandom(min, max, howMuch, arrayName) {
   }
   console.log(arrayName);
 }
+
+// funzione per comparare gli array fra di loro e vedere se sono stati inseriti gli stessi valori
+function arrayCompare(_arr1, _arr2) {
+  // se array 1 e array 2 non sono degli array e se la loro lungheza non è uguale mi ritorna falso
+  if (
+    !Array.isArray(_arr1) ||
+    !Array.isArray(_arr2) ||
+    _arr1.length !== _arr2.length
+  ) {
+    return false;
+  }
+  // variabili per ordinare gli array con concat unisco tutti i valori dell'array e con sort li metto in ordine
+  const arr1 = _arr1.concat().sort();
+  const arr2 = _arr2.concat().sort();
+
+  //ciclo per fare il check dei valori degli array da comparare
+  for (let i = 0; i < arr1.length; i++) {
+    // se i valori dell array 1 sono diversi dai valori dell'array 2 allora la funzione mi ritorna falso
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  // se tutte le condizioni sopra sono passate allora la funzione mi ritornerà un true
+  return true;
+}
+
+// funzione per trovare i numeri che sono stati indovinati
+function findMatches(arr1, arr2) {
+  return arr1.filter((num) => arr2.includes(num));
+}
--- js/main.js
+++ js/main.js
@@ -10,6 +10,39 @@ generateTotNumRandom(1, 100, 5, randomNumArr);
 // creo un ciclo per estrapolare i singoli elementi dell'array e stamparli in pagina
 for (let i = 0; i < randomNumArr.length; i++) {
   let risultato = randomNumArr[i];
-  console.log(risultato);
+  //   console.log(risultato);
   numContainer.innerHTML += `<span class="random-num">${risultato}</span>`;
 }
+
+// creo un timeout di 30 secondi dopo i quali scompaiono i numeri generati e chiedo all'utente tramite il prompt in che ordine erano i numeri
+setTimeout(() => {
+  // rendo invisibili i numeri dopo il tempo stimato
+  numContainer.innerHTML = "";
+  setTimeout(() => {
+    // creo un array vuoto dove verranno inseriti i numeri scelti dall'utente
+    let userNumbers = [];
+    for (i = 1; i <= 5; i++) {
+      let userInput = parseInt(
+        prompt(`Inserisci il numero che era nella posizione: ${i}`)
+      );
+      userNumbers.push(userInput);
+    }
+    console.log(userNumbers);
+
+    // Trovo i numeri indovinati
+    let matches = findMatches(randomNumArr, userNumbers);
+    console.log("Numeri indovinati:", matches);
+    alert(`Hai indovinato ${matches.length} numeri: ${matches.join(", ")}`);
+
+    // creo una variabile con dentro la funzione per il check degli array
+    let arrayCheck = arrayCompare(randomNumArr, userNumbers);
+    console.log(arrayCheck);
+
+    // creo le condizioni dove se arraycheck è false l'user ha perso altrimenti ha vinto
+    if (arrayCheck === false) {
+      alert("HAI PERSO");
+    } else {
+      alert("HAI VINTO");
+    }
+  }, 1);
+}, 30000);
